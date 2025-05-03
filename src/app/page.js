81b531db5e96735ca461c39912e4015b5218abd4
viml kumar <vimalkg@gmail.com>
2025-05-03 13:39:03 +0530

    wzk.vercel.app mck82.vercel.app fb.com/ztrabc x.com/etphor

--- src/app/page.js
+++ src/app/page.js
@@ -37,14 +37,12 @@ steps to use : 1. pls replace ԃis tekst to indiα/nepαl/sinhl/bαnglα.
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
       <div className="flex gap-4 items-center flex-col sm:flex-row"><Hsciifontpicker/></div>
         <div className="flex gap-1 items-center flex-col sm:flex-row">
-			<Button startContent={<Image src="/flg8.jpg" alt="modern india languages" width={100} height={100}/>}
+			<Button startContent={<Image src="/flg8.png" alt="modern india languages" width={100} height={100}/>}
 				className="border-black border-2" onPress={on_eu2l}></Button>
-			<Button startContent={<Image src="/xvJQ.jpg" alt="modern india languages" width={100} height={100}/>}
-				className="border-black border-2" onPress={on_xvjqK2u}></Button>
-			<Button startContent={<Image src="/jqh.jpg" alt="modern india languages" width={100} height={100}/>}
-			 className="border-black border-2" onPress={on_kh2uK}></Button>
+			<Button className="border-black border-2" onPress={on_xvjqK2u}><vxQKDJTZ/Button>
+			<Button className="border-black border-2" onPress={on_kh2uK}>thqhkhjhzh2TQKJZ</Button>
 			<a className="border-black border-2" href="https://github.com/zawa8/font" target="_blank" rel="noopener noreferrer"
-			><Image src="/4fngronli.png" alt="modern india languages" width={100} height={100}/></a>
+			>free hscii font</a>
         </div>
         <div className="flex min-w-full gap-2 items-center flex-col sm:flex-row">
           <Textarea id="ita"  rows={12} className="w-full border-black" value={iteksta}  onChange={handle_ita_change} placeholder={iplaceholer} />
